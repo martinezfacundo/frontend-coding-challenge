36822f9c7949563e108d24f01b102a92640d9fa5
Facundo Martinez <martinezfaacu@gmail.com>
2021-07-30 18:21:51 -0300

    update styles, page functions

--- src/components/reposCardContainer/ReposCardContainer.js
+++ src/components/reposCardContainer/ReposCardContainer.js
@@ -4,26 +4,47 @@ import './ReposCardContainer.css'
 
 function ReposCardContainer() {
 
-    const [page, setPage] = useState(1)
+    const [page, setPage] = useState(34)
     const [repos, setRepos] = useState([])
 
+    // Async function to get the info of repositorys created between 2017-10-22 and 2017-11-22, using the state "page" to change the page
     const getRepos = async () => {
         const response = await fetch(`https://api.github.com/search/repositories?q=created:2017-10-22..2017-11-22&sort=stars&order=desc&page=${page}`);
         const data = await response.json()
         setRepos(data.items)
     }
 
+    const nextPage = () => {
+        if(repos){
+            setPage(page + 1)
+        }
+    }
+
+    const prevPage = () => {
+        if(page > 1) {
+            setPage(page - 1)
+        }
+    }
+
     useEffect(() => {
         getRepos()
     }, [page])
 
-    console.log(repos)
-    repos.map(elem => console.log(elem.name))
-
     return (
-        <div className='container'>
-            {repos.map(elem => <RepoCard repositoryData={elem}/>)}
-        </div>
+            <div className='cards-container'>
+                <div className='page-functions-container'>
+                    <div className='page-count'>Page: {page}</div>
+                    <button onClick={prevPage} className='page-button'>Prev</button>
+                    <button onClick={nextPage} className='page-button'>Next</button>
+                </div>
+                {/* If repositories info does not exist, an announcement is shown*/}
+                {repos? repos.map(elem => <RepoCard repositoryData={elem}/>) : <h1 className='no-results'>apparently there are no more results :(</h1>}
+                <div className='page-functions-container'>
+                    <div className='page-count'>Page: {page}</div>
+                    <button className='page-button'>Prev</button>
+                    <button className='page-button'>Next</button>
+                </div>
+            </div>
     )
 }
 
